1f001ba1276c3e2ff7bf1ec83b824b4460d10f2f
Daniel Hernández <josephdzr@gmail.com>
2020-01-03 15:22:01 -0600

    Problem 2

---
+++ p2.hs
@@ -0,0 +1,16 @@
+main :: IO ()
+main = do
+  putStrLn . show $ filterFib 4000000
+
+filterFib :: Int -> Int
+filterFib n = sum $ filter even (fib 0 1 n)
+
+fib :: Int -> Int -> Int -> [Int]
+fib a b c
+  | a == 0 && b == 1 = 1:(fib 1 1 c)
+  | a == 1 && b == 1 = 1:(fib 1 2 c)
+  | b > c = []
+  | otherwise = (b):(fib b (a + b) c)
+
+
+
