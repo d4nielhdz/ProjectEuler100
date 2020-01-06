a93be681385c77b86e364b52ed54eed95f6f1926
Daniel Hernández <josephdzr@gmail.com>
2020-01-05 18:49:32 -0600

    Problem 3

--- p3.hs
+++ p3.hs
@@ -1,37 +1,10 @@
-import qualified Data.Set as PQ
-
 main :: IO ()
 main = do
-  putStrLn . show $ largestPrimeFactor 600851475143
-  
-largestPrimeFactor :: Int -> Int
-largestPrimeFactor x = takeFirst (\n -> mod x n == 0) (reverse $ primesUpToN x)
+  putStrLn . show $ lPF 600851475143 2 1
 
-takeFirst :: (a -> Bool) -> [a] -> a
-takeFirst f (x:xs)
-  | f x == True = x
-  | otherwise   = takeFirst f xs
-
--- This function was written by Melissa E. O’Neill. Check out her
--- paper about it at https://www.cs.hmc.edu/~oneill/papers/Sieve-JFP.pdf
-primes :: [Int]
-primes = 2:sieve [3,5..]
-  where
-    sieve (x:xs) = x : sieve' xs (insertprime x xs PQ.empty)
-
-    sieve' (x:xs) table
-        | nextComposite == x = sieve' xs (adjust x table)
-        | otherwise          = x : sieve' xs (insertprime x xs table)
-      where 
-        (nextComposite,_) = PQ.findMin table
-
-    adjust x table
-        | n == x    = adjust x (PQ.insert (n', ns) newPQ)
-        | otherwise = table
-      where
-        Just ((n, n':ns), newPQ) = PQ.minView table
-
-    insertprime p xs = PQ.insert (p*p, map (*p) xs)
-
-primesUpToN :: Int -> [Int]
-primesUpToN n = takeWhile (\x -> x <= n) primes
+-- Implementation taken from https://math.stackexchange.com/questions/389675/largest-prime-factor-of-600851475143
+lPF :: Integer -> Integer -> Integer -> Integer
+lPF x y z
+  | x == 1 = z
+  | mod x y == 0 = lPF (div x y) 2 (max y z)
+  | otherwise = lPF x (y+1) z
