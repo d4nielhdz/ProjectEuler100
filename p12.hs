7e46b498067f5d87e9a22563aebdaa9456b1e9a0
Daniel Hernández <josephdzr@gmail.com>
2020-01-07 23:53:25 -0600

    Problem 13

--- p12.hs
+++ p12.hs
@@ -7,10 +7,12 @@ triangulars :: [Int]
 triangulars = tri' 0 0
   where  tri' x y = (x + y) : tri' (x + y) (y + 1)
 
-factors :: Int -> Int -> [Int]
-factors _ 0 = []
-factors _ 1 = 1:[]
-factors x y = if mod x y == 0 then y:(factors x (y - 1)) else factors x (y-1)
+factors :: Int -> [Int]
+factors x = facs x x
+  where
+    facs _ 0 = []
+    facs _ 1 = 1:[]
+    facs x y = if mod x y == 0 then y:(facs x (y - 1)) else facs x (y-1)
 
 triNumWithNFactors :: Int -> Int
-triNumWithNFactors n = takeFirst (\x -> length (factors x x) > n) triangulars
+triNumWithNFactors n = takeFirst (\x -> length (factors x) > n) triangulars
